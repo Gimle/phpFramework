082cf15474de0adb23cb30596f56b193a2b82f4a
Tux Solbakk <tux@krageroby.no>
2017-05-16 19:44:44 +0200

    Migrated the translation system with the android backend.

---
+++ lib/gimle/i18n.php
@@ -0,0 +1,120 @@
+<?php
+declare(strict_types=1);
+namespace gimle;
+
+/**
+ * Internatiolization.
+ */
+class i18n
+{
+	use trick\Singelton;
+
+	/**
+	 * A local holder for the configuration.
+	 *
+	 * @var ?array
+	 */
+	private $config = null;
+
+	/**
+	 * The current active language.
+	 *
+	 * @var ?string
+	 */
+	private $language = null;
+
+	/**
+	 * A holder for the translation objects.
+	 *
+	 * @var mixed
+	 */
+	private $objects = [];
+
+	/**
+	 * Set up the class.
+	 */
+	private function __construct ()
+	{
+		$this->config = Config::get('i18n');
+		if ((!isset($this->config['lang'])) || (!is_array($this->config['lang'])) || (empty($this->config['lang']))) {
+			throw new Exception('No language configured.');
+		}
+	}
+
+	/**
+	 * Sets up the current i18n instance.
+	 *
+	 * @return void
+	 */
+	private function setup (): void
+	{
+		if (isset($this->config['lang'][$this->language]['lc'])) {
+			setlocale(LC_TIME, $this->config['lang'][$this->language]['lc']);
+			setlocale(LC_COLLATE, $this->config['lang'][$this->language]['lc']);
+			setlocale(LC_CTYPE, $this->config['lang'][$this->language]['lc']);
+		}
+
+		if ((isset($this->config['lang'][$this->language]['objects'])) && (is_array($this->config['lang'][$this->language]['objects'])) && (!empty($this->config['lang'][$this->language]['objects']))) {
+			foreach ($this->config['lang'][$this->language]['objects'] as $object => $params) {
+				$this->objects[] = call_user_func_array([__NAMESPACE__ . '\\i18n\\' . mb_ucfirst($object), 'getInstance'], [$this->language, $params]);
+			}
+		}
+	}
+
+	/**
+	 * Set the language.
+	 *
+	 * @param ?string $language The language
+	 * @return void
+	 */
+	public function setLanguage (?string $language = null): void
+	{
+		if ($language === null) {
+			$this->language = get_preferred_language(array_keys($this->config['lang']));
+			if ($this->language === null) {
+				if ((isset($this->config['default'])) && (isset($this->config['lang'][$this->config['default']]))) {
+					$this->language = $this->config['default'];
+				}
+				else {
+					$this->language = current(array_keys($this->config['lang']));
+				}
+			}
+		}
+		else {
+			if (isset($this->config['lang'][$language])) {
+				$this->language = $language;
+			}
+			elseif ((isset($this->config['default'])) && (isset($this->config['lang'][$this->config['default']]))) {
+				$this->language = $this->config['default'];
+			}
+			else {
+				$this->language = current(array_keys($this->config['lang']));
+			}
+		}
+
+		$this->setup();
+	}
+
+	/**
+	 * Translate a message.
+	 *
+	 * @param mixed $message The message to be translated.
+	 * @return mixed The translated message
+	 */
+	public function _ (...$message)
+	{
+		if ($this->language === null) {
+			$this->setLanguage();
+		}
+
+		if (!empty($this->objects)) {
+			foreach ($this->objects as $object) {
+				$result = $object->_(...$message);
+				if ($result !== null) {
+					return $result;
+				}
+			}
+		}
+		return current($message);
+	}
+}
---
+++ lib/gimle/i18n/android.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+namespace gimle\i18n;
+
+use gimle\xml\SimpleXmlElement;
+
+/**
+ * A class for handling translations stored in Android compatible xml files.
+ */
+class Android
+{
+	use \gimle\trick\Multiton;
+
+	/**
+	 * The SimpleXmlElement objects used by this class.
+	 *
+	 * @var array<gimle\xml\SimpleXmlElement>
+	 */
+	private $sxml = [];
+
+	/**
+	 * Load the translation files.
+	 *
+	 * @throws gimle\ErrorException If the xml file is not found.
+	 * @param string $identifier
+	 * @param mixed $params
+	 */
+	public function __construct (string $identifier, $params)
+	{
+		if (is_string($params)) {
+			$params = [$params];
+		}
+
+		foreach ($params as $file) {
+			$this->sxml[] = new SimpleXmlElement(file_get_contents($file));
+		}
+	}
+
+	/**
+	 * Translate a message.
+	 *
+	 * @param mixed ...$message
+	 * @return mixed
+	 */
+	public function _ (...$message)
+	{
+		$properties = [];
+		if (is_array(end($message))) {
+			$properties = array_pop($message);
+			if ((isset($properties['quantity'])) && (is_int($properties['quantity']))) {
+				if ($properties['quantity'] === 1) {
+					$properties['quantity'] = 'one';
+				}
+				else {
+					$properties['quantity'] = 'other';
+				}
+			}
+		}
+		foreach ($message as $lookup) {
+			if (isset($properties['quantity'])) {
+				$query = '//plurals[@name=' . current($this->sxml)->real_escape_string($lookup) . ']/item[@quantity="' . $properties['quantity'] . '"]';
+			}
+			elseif (in_array('array', $properties)) {
+				$query = '//string-array[@name=' . current($this->sxml)->real_escape_string($lookup) . ']';
+			}
+			else {
+				$query = '//string[@name=' . current($this->sxml)->real_escape_string($lookup) . ']';
+			}
+			foreach ($this->sxml as $sxml) {
+				$result = current($sxml->xpath($query));
+				if ($result !== false) {
+					if (in_array($result->getName(), ['string', 'item'])) {
+						$result = current($result->innerXml());
+						if ($result === false) {
+							return null;
+						}
+						if ((substr($result, 0, 1) === '"') && (substr($result, -1, 1) === '"')) {
+							return substr($result, 1, -1);
+						}
+						return str_replace(['\\\'', '\\"'], ['\'', '"'], $result);
+					}
+
+					$return = [];
+					foreach ($result->xpath('./item') as $item) {
+						$result = current($item->innerXml());
+						if ($result === false) {
+							return null;
+						}
+						if ((substr($result, 0, 1) === '"') && (substr($result, -1, 1) === '"')) {
+							$return[] = substr($result, 1, -1);
+						}
+						$return[] = str_replace(['\\\'', '\\"'], ['\'', '"'], $result);
+					}
+					return $return;
+				}
+			}
+		}
+		return null;
+	}
+}
