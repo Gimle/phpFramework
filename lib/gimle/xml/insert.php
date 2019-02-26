40e61602db257df82dac34105f088352ba15e1b7
Tux Solbakk <tux@krageroby.no>
2019-02-26 16:01:33 +0100

    Fix to be able to insert to root node.

--- lib/gimle/xml/insert.php
+++ lib/gimle/xml/insert.php
@@ -58,12 +58,10 @@ trait Insert
 				}
 			}
 			elseif ($mode === self::BLOCK) {
+				$leadingWhitespace = '';
 				if ($dom->parentNode->nodeValue !== null) {
 					preg_match('/^[\s]+/s', $dom->parentNode->nodeValue, $leadingWhitespace);
-					$leadingWhitespace = ($leadingWhitespace[0] ?? null);
-				}
-				else {
-					$leadingWhitespace = '';
+					$leadingWhitespace = ($leadingWhitespace[0] ?? '');
 				}
 				$insertBefore = $leadingWhitespace . "\t";
 
@@ -124,9 +122,12 @@ trait Insert
 				}
 			}
 			elseif ($mode !== self::INLINE) {
-				preg_match('/^[\s]+/s', $dom->parentNode->nodeValue, $leadingWhitespace);
-				$leadingWhitespace = ($leadingWhitespace[0] ?? null);
-				$insertAfter = $leadingWhitespace;
+				$leadingWhitespace = '';
+				if ($dom->parentNode->nodeValue !== null) {
+					preg_match('/^[\s]+/s', $dom->parentNode->nodeValue, $leadingWhitespace);
+					$leadingWhitespace = ($leadingWhitespace[0] ?? '');
+					$insertAfter = $leadingWhitespace;
+				}
 
 				$node = new \DomDocument();
 				$node = $node->createTextNode($leadingWhitespace . "\t");
