d8c8f763ed996228972c3d5dd73e09ced597093b
Tux Solbakk <tux@krageroby.no>
2021-07-31 14:09:15 +0000

    Trigger user function after sign out.

--- canvas/processsignout.php
+++ canvas/processsignout.php
@@ -7,6 +7,10 @@ if (isset($_SESSION['gimle']['user'])) {
 	unset($_SESSION['gimle']['user']);
 }
 
+if (method_exists(__NAMESPACE__ . '\\User', 'postSignOut')) {
+	User::postSignOut();
+}
+
 User::setCookie('Asi', 'false', time() - (86400 * 400));
 
 header('Location: ' . BASE_PATH);
