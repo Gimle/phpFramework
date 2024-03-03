ea7957a2ac67de4050e5c674a3686ed1778c9fbe
Tux Solbakk <tux@krageroby.no>
2024-03-03 21:44:20 +0100

    Better gitup info.

--- cli/gitup.php
+++ cli/gitup.php
@@ -21,6 +21,11 @@ php ./module/gimle/cli/gitup.php
  */
 define('gimle\\SITE_DIR', dirname(__DIR__, 3) . DIRECTORY_SEPARATOR);
 
+$cli = [
+	'description' => 'Update all submodules tho their set branch.',
+	'options' => [],
+];
+
 require dirname(__DIR__) . '/init.php';
 
 $exec = 'cd ' . SITE_DIR . '; git pull';
@@ -28,11 +33,12 @@ exec($exec);
 
 $gitmodules = parse_config_file(SITE_DIR . '.gitmodules', false);
 foreach ($gitmodules as $name => $config) {
+	echo colorize($name, 'string') . "\n";
 	if (!isset($config['branch'])) {
 		echo "{$config['path']} does not have a branch to follow.\n";
 		continue;
 	}
 
 	$exec = 'cd ' . SITE_DIR . $config['path'] . '; git checkout ' . $config['branch'] . '; git pull';
-	exec($exec);
+	passthru($exec);
 }
